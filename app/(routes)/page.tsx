5bfc3e24b4274ee8c093390dbce538b7d191e8e7
Yatik Srivastava <yatiksrivastava1@gmail.com>
2024-04-26 13:15:00 +0530

    page

--- app/(routes)/page.tsx
+++ app/(routes)/page.tsx
@@ -1,7 +1,17 @@
+import getBillboard from "@/actions/get-billboard"
+import Billboard from "@/components/billboard"
+import Container from "@/components/ui/container"
 
-const HomePage = () => {
+export const revalidate = 0
+
+const HomePage = async () => {
+  const billboard = await getBillboard("dad50feb-b538-453e-a846-453ebffcf8c7")
   return (
-    <div>Hello Store</div>
+    <Container>
+      <div className="space-y-10 pb-10">
+         <Billboard data={billboard} />
+      </div>
+    </Container>
   )
 }
 
