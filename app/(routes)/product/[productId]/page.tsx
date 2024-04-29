007f5602f905d484d3c1ee55fbb700c226025081
Yatik Srivastava <yatiksrivastava1@gmail.com>
2024-04-29 12:20:05 +0530

    product page

--- app/(routes)/product/[productId]/page.tsx
+++ app/(routes)/product/[productId]/page.tsx
@@ -1,6 +1,7 @@
 import getProduct from "@/actions/get-product"
 import getProducts from "@/actions/get-products"
 import Gallery from "@/components/gallery"
+import Info from "@/components/info"
 import Container from "@/components/ui/container"
 import ProductList from "@/components/ui/product-list"
 
@@ -20,13 +21,13 @@ const ProductPage : React.FC<ProductPageProps> = async ({
   })
 
   return (
-     <div className="bg-white">\
+     <div className="bg-white">
        <Container>
          <div className="px-4 py-10 sm:px-6 lg:px-8">
             <div className="lg:grid lg:grid-cols-2 lg:items-start lg:gap-x-8">
                 <Gallery images={product.images} />
                 <div className="mt-10 px-4 sm:mt-16 sm:px-0 lg:mt-0">
-                    Info
+                    <Info data={product} />
                 </div>
             </div>
             <hr className="my-10" />
