1c7fe381bff4dec16a89c52c601e4f4c376d1b01
Yatik Srivastava <yatiksrivastava1@gmail.com>
2024-04-30 12:58:06 +0530

    debug

--- actions/get-product.tsx
+++ actions/get-product.tsx
@@ -2,12 +2,12 @@ import { Product } from "@/types"
 
 
 const getProduct = async ( id : string ) : Promise<Product> => {
-    const URL = `${process.env.PUBLIC_API_URL}/products/${id}`
+    const URL = `${process.env.PUBLIC_API_URL}/products/${id}`;
     console.log(URL);
     
-    const res = await fetch(URL)
+    const res = await fetch(URL);
 
     return res.json();
 } 
 
-export default getProduct
+export default getProduct;
