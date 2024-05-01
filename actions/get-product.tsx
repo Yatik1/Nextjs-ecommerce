09649874a3bea4d24126b55388765120d801b9e4
Yatik Srivastava <yatiksrivastava1@gmail.com>
2024-05-01 13:12:37 +0530

    delete log

--- actions/get-product.tsx
+++ actions/get-product.tsx
@@ -3,7 +3,6 @@ import { Product } from "@/types"
 
 const getProduct = async ( id : string ) : Promise<Product> => {
     const URL = `${process.env.PUBLIC_API_URL}/products/${id}`;
-    console.log(URL);
     
     const res = await fetch(URL);
 
